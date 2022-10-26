5a1af590dad9c7573e568c8bde02c83cb484799a
mahmoud salem <mahmudsalem.eg@gmail.com>
2022-10-26 03:47:30 +0200

    -

--- app/Http/Controllers/Dashboard/SubscriptionController.php
+++ app/Http/Controllers/Dashboard/SubscriptionController.php
@@ -11,19 +11,16 @@ class SubscriptionController extends Controller
 {
     public function showSubscriptions(Request $request)
     {
-        $pageConfigs = [
+        $data['pageConfigs'] = [
             'pageHeader' => false,
             'defaultLanguage' => 'ar',
             'direction' => 'rtl'
         ];
-        $title = trans('admin.subscription_title');
+        $data['title'] = trans('admin.subscription_title');
         $data['debatable_names'] = array(trans('admin.email'), trans('admin.phone'),
             trans('admin.full_name_ar'), trans('admin.full_name_en'), trans('admin.sport_name'),
             trans('admin.nationality'), trans('admin.actions'));
-        return view('admin.subscription.index', [
-            'pageConfigs' => $pageConfigs,
-            'title' => $title,
-        ]);
+        return view('admin.subscription.index')->with($data);
     }
 
     public function getSubscriptionsData(Request $request)
