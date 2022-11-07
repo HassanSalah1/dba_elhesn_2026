4bd42b4586775402e49202226d73dfe54ea4403e
mahmoud salem <mahmudsalem.eg@gmail.com>
2022-11-07 19:23:00 +0200

    -

--- app/Console/Commands/GetPlayerPhotos.php
+++ app/Console/Commands/GetPlayerPhotos.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Models\TeamPlayer;
 use App\Repositories\Api\Setting\SettingApiRepository;
 use App\Repositories\Api\SqlServerApiRepository;
+use App\Repositories\General\UtilsRepository;
 use Illuminate\Console\Command;
 
 class GetPlayerPhotos extends Command
@@ -44,7 +45,17 @@ class GetPlayerPhotos extends Command
         if ($conn) {
             $players = TeamPlayer::where('image', '=', null)->get();
             foreach ($players as $player) {
-                $image = SqlServerApiRepository::getPlayerImage($conn, $player->player_id);
+                $playerId = $player->player_id;
+                $image = null;
+                $sql = "SELECT TOP 1 PlayerPhoto FROM dbo.MobileApp_PlayersPhotos WHERE PlayerRowID=$playerId";
+                $file_id = 'IMG_' . mt_rand(00000, 99999) . (time() + mt_rand(00000, 99999));
+                $image_path = 'uploads/players/';
+                if (($result = \sqlsrv_query($conn, $sql)) !== false) {
+                    $object = sqlsrv_fetch_object($result);
+                    if ($object) {
+                        $image = UtilsRepository::createImageBase64(base64_encode($object->PlayerPhoto), $image_path, $file_id, 282, 561);
+                    }
+                }
                 $this->info('image : ' . $image);
                 if ($image) {
                     $player->update([
