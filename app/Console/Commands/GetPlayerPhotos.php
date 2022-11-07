c846d8105081a23c7da2174843f127abe4d1aa83
mahmoud salem <mahmudsalem.eg@gmail.com>
2022-11-07 19:43:52 +0200

    -

--- app/Console/Commands/GetPlayerPhotos.php
+++ app/Console/Commands/GetPlayerPhotos.php
@@ -41,30 +41,7 @@ class GetPlayerPhotos extends Command
      */
     public function handle()
     {
-        $conn = SqlServerApiRepository::startConnection();
-        if ($conn) {
-            $players = TeamPlayer::where('image', '=', null)->get();
-            foreach ($players as $player) {
-                $playerId = $player->player_id;
-                $image = null;
-                $sql = "SELECT TOP 1 PlayerPhoto FROM dbo.MobileApp_PlayersPhotos WHERE PlayerRowID=$playerId";
-                $file_id = 'IMG_' . mt_rand(00000, 99999) . (time() + mt_rand(00000, 99999));
-                $image_path = 'uploads/players/';
-                if (($result = \sqlsrv_query($conn, $sql)) !== false) {
-                    $object = sqlsrv_fetch_object($result);
-                    if ($object) {
-                        $this->info('sss : '. base64_encode($object->PlayerPhoto));
-                        $image = UtilsRepository::createImageBase64(base64_encode($object->PlayerPhoto), $image_path, $file_id, 282, 561);
-                    }
-                }
-                $this->info('image : ' . $image);
-                if ($image) {
-                    $player->update([
-                        'image' => $image
-                    ]);
-                }
-            }
-        }
+        SqlServerApiRepository::getPlayerImages();
         $this->info('Successfully');
         return Command::SUCCESS;
     }
