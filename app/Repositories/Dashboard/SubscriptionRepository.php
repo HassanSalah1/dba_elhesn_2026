f8e1c14da0a18ba97d3f5436bbadcf0cc3082685
mahmoud salem <mahmudsalem.eg@gmail.com>
2022-12-08 00:34:54 +0200

    -

--- app/Repositories/Dashboard/SubscriptionRepository.php
+++ app/Repositories/Dashboard/SubscriptionRepository.php
@@ -17,7 +17,7 @@ class SubscriptionRepository
             })
             ->addColumn('actions', function ($subscription) {
                 $ul = '';
-                $ul .= '<a data-toggle="tooltip" title="' . trans('admin.show') . '" id="' . $subscription->id . '" href="' . url('/sport/subscription/show/' . $subscription->id) . '" class="on-default edit-row btn btn-info"><i data-feather="eye"></i></a>
+                $ul .= '<a data-toggle="tooltip" title="' . trans('admin.show') . '" id="' . $subscription->id . '" href="' . url('/admin/sport/subscription/show/' . $subscription->id) . '" class="on-default edit-row btn btn-info"><i data-feather="eye"></i></a>
                    ';
                 return $ul;
             })->make(true);
