e9add481f1c7e4d8288401e8ab0d61be017c2f42
mahmoud salem <mahmudsalem.eg@gmail.com>
2023-01-07 17:05:12 +0200

    -

--- app/Http/Middleware/AuthWare.php
+++ app/Http/Middleware/AuthWare.php
@@ -11,7 +11,7 @@ class AuthWare
     public function handle($request, Closure $next, $role = null)
     {
         $user = \auth()->user();
-        if (!$user || $user->role !== UserRoles::ADMIN) {
+        if (!$user || (!in_array($user->role, [UserRoles::EMPLOYEE, UserRoles::ADMIN]))) {
             return redirect()->to(url('/admin/auth/login'));
         }
         return $next($request);
