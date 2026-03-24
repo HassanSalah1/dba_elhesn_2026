2e42f017d637ef476b84f6a527f254cf5b2f1fe2
hassansalah <hs8266@gmail.com>
2026-03-24 12:23:05 +0200

    Add trust_server_certificate option in database configuration for SQL Server connections to enhance security and compatibility.

--- config/database.php
+++ config/database.php
@@ -100,6 +100,7 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
+            'trust_server_certificate' => true,
         ],
 
     ],
